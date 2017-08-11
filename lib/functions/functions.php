f0bf38bb07c70282db34d1432f791f2af5b71ed9
AlbinGasi <albin.g@live.com>
2017-08-11 21:50:11 +0200

    Update

--- lib/functions/functions.php
+++ lib/functions/functions.php
@@ -2,12 +2,15 @@
 
 function set_header_script(){
 	echo set_script('https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
-	echo set_script('/public/js/angular.min.js');
-	echo set_script('/public/js/angular-route.min.js');
+	//echo set_script('/public/js/angular.min.js');
+	//echo set_script('/public/js/angular-route.min.js');
+	echo set_style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
+	echo set_style('/public/css/index/style.css');
 }
 
 function set_footer_script(){
 	echo set_script('https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+	echo set_script('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
 }
 
 
